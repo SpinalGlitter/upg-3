4c3e6d03ce18546032c20bff873809c1d06e4823
Johannes <johannes.persson@lerniastudent.se>
2024-09-15 20:52:38 +0200

    sun

--- upg3.js
+++ upg3.js
@@ -1,1 +1,15 @@
-const myBody = document.querySelector("body")
+const myBody = document.querySelector("body");
+let buttons= document.getElementsByTagName("button");
+let body= document.getElementsByClassName("changecolor");
+
+for(let i= 0; 1< buttons.length; i++){
+ let button = buttons[i];
+ let color;
+ button.addEventListener("click", function() {
+    color = buttons.className.toString();
+    color = color.split("button")[0];
+    body.style.color = color;
+ }
+ )
+;
+}
