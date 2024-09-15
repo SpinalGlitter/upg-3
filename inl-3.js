f3484565771d096030ccc9fffa24157ec5d5a248
Johannes <johannes.persson@lerniastudent.se>
2024-09-15 22:32:46 +0200

    news

---
+++ inl-3.js
@@ -0,0 +1,21 @@
+function change() {
+    let elem = document.getElementById("change-text");
+
+    if (elem.innerHTML=="Hello World!") {
+        elem.innerHTML = "Goodbye World!";
+    }
+   else {
+    elem.innerHTML = "Hello World!"
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
